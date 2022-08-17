fad26e52f2001cd175b919be91a11208fcece340
msaadat03 <saadatam.code.edu.az>
2022-08-17 22:03:08 +0300

    HomeW13

---
+++ assets/js/script.js
@@ -0,0 +1,138 @@
+// var name = "Saadat";
+// var surname = "Mursaliyeva";
+
+// let name;
+// name = "Sada"
+
+
+// let name1 = "Sada";
+// let name2 = "Sado";
+
+// const age = 18;
+// console.log(age)
+
+// console.log(name1 + name2)
+
+// console.log(name + surname);
+// console.log(name + "" + surname);
+// console.log(`${name} ${surname}`);
+
+let age = 18;
+let surname = "Mursaliyeva";
+let isMarried = true;
+let address = null;
+let count;
+
+
+let person1 = {
+    name: "Saadat",
+    surname: "Mursaliyeva",
+    age: 18,
+    phoneNumbers: [15645]
+}
+
+let person2 = {
+    name: "Fidan",
+    surname: "Merdanli",
+    age: 17,
+    phoneNumbers: [75894]
+
+}
+
+let students = [person1, person2];
+
+
+// let students = ["Sada", "Sado", 18];
+
+// for (let i=0; i < students.length; i++) {
+//     console.log(students[1])
+// }
+
+
+//  for (let i=0; i < students.length; i++) {
+//     for (let j = 0; j < students[i].phoneNumbers.length; j++) {
+
+//         console.log(students[i].phoneNumbers[j]);
+
+//     }
+//  }
+
+// console.log(person.name + "" + person.surname);
+
+// for (let i=0; i < person1.personNumbers[i]; i++) {
+//     console.log(person1.PhoneNumbers[i]);
+// }
+
+// let numbers = [3,4,5,6,7,8]
+
+// numbers.forEach(item => {
+//     console.log(item);
+// }
+// )
+
+
+// let num1 = 65;
+// let num2 = 33;
+
+// let result = num1 % num2;
+
+// console.log(result);
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i == 0) {
+//     console.log("Ne tek ne de cutdur");
+//   } else {
+//     if (i % 2 == 1) {
+//       console.log("Tek:" + i);
+//     } else {
+//       console.log("Cut:" + i);
+//     }
+//   }
+// }
+
+// let isActive = true;
+
+// if (!isActive) {
+//     console.log("Aktiv deyil");
+// }else{
+//     console.log("Aktivdir");
+// }
+
+// let studentSurname = null;
+
+// let result = studentSurname ?? "Is surname forget";
+
+// let result = isActive ? "Aktivdir" : "Aktiv deyil";
+
+// console.log(result);
+
+// let a = 5;
+// let b = a;
+// b = 8;
+
+// console.log(a);
+// console.log(b);
+
+// let obj1 = {
+//   name: "Saadat",
+//   surname: "Mursaliyeva",
+//   age: 18,
+//   phoneNumbers: [567, 765],
+// };
+
+// let obj2 = obj1;
+// obj2.age = 55;
+
+// console.log(obj1);
+
+// let studentName;
+
+// if (studentName == undefined) {
+//     console.log("Teyin olunmayib");
+// }else{
+//     console.log("Teyin olunub");
+// }
+
+// let isDeleted = null;
+
+// console.log(typeof isDeleted);
